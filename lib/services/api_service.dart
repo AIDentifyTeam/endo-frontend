292bcff1b6c75a10be416acb40b8a80c7ceb5843
ErfanMN <mahmoudinia.erfan@gmail.com>
2025-07-20 19:27:43 +0330

    fix baseurl

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,8 +8,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
-// const String baseUrl = 'https://aidentify.app';
-const String baseUrl = 'http://127.0.0.1:8000';
+const String baseUrl = 'https://aidentify.app';
+// const String baseUrl = 'http://127.0.0.1:8000';
 
 class ApiService {
   final String apiUrl = '$baseUrl/api';
