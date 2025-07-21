8a09762d1774d9051521b3c5c1dba77a738d720c
ErfanMN <mahmoudinia.erfan@gmail.com>
2025-07-21 10:25:46 +0330

    fix some naming configurations

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:endo_frontend/models/patient.dart';
 import 'package:endo_frontend/models/visit.dart';
 import 'package:endo_frontend/screens/patient_profile.dart';
--- lib/widgets/patient_info_card.dart
+++ lib/widgets/patient_info_card.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:endo_frontend/models/patient.dart';
 
